513d32a5f5ca9c61368e8caf126851b91e30f429
alphabstc <51530816+alphabstc@users.noreply.github.com>
2020-11-18 22:57:54 +0800

    upload

---
+++ Assets/Scripts/AreaCollider.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaCollider : MonoBehaviour
+{
+    public int sign = 0;
+    FirstSceneController sceneController;
+    private void Start(){sceneController = GameDirector.GetInstance().CurrentScenceController as FirstSceneController;}
+    void OnTriggerEnter(Collider collider){if (collider.gameObject.name == "Player(Clone)")sceneController.wall_sign = sign;}
+}
---
+++ Assets/Scripts/GameDirector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : System.Object
+{
+    private static GameDirector _instance;             //导演类的实例
+    public ISceneController CurrentScenceController { get; set; }
+    public static GameDirector GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new GameDirector();
+        }
+        return _instance;
+    }
+}
